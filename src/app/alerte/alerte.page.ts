1312dae94e8c2d5336d462d77d3dac744e8dd3a6
Romane <rcotecoliss@ensc.fr>
2023-04-22 01:14:15 +0200

    modif alerte en cours

--- src/app/alerte/alerte.page.ts
+++ src/app/alerte/alerte.page.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { AlertController } from '@ionic/angular';
+import { Observable, async } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-alerte',
   templateUrl: 'alerte.page.html',
-  styleUrls: ['alerte.page.scss']
+  styleUrls: ['alerte.page.scss'],
 })
 export class AlertePage {
+  items: Observable<any[]>;
 
-  constructor() {}
+  constructor(
+    public firestore: AngularFirestore,
+    public alertController: AlertController
+  ) {
+    this.items = this.firestore
+      .collection('treatment', (ref) => ref.where('Alarm', '==', true))
+      .valueChanges();
+  }
 
+  // Modification d'un élément
+  //configuration de la fenêtre pop-up
+  
+  async updateAlert(id: string, Alarm: boolean, Hour: string) {
+    const alert = await this.alertController.create({
+      header: 'Modifier une alerte',
+      message: "Sélectionner les alertes que vous voulez activer",
+      inputs: [
+        {
+          name: 'Alarm',
+          type: 'radio',
+          label: 'doliprane',
+          value: true,
+        },
+        {
+          name: 'Alarm',
+          type: 'radio',
+          label: 'Desactivée',
+          value: false,
+        },
+        {
+          name: 'Hour',
+          type: 'text',
+          placeholder: 'Heure de l\'alerte',
+        }
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+        },
+        {
+          text: 'Ajouter',
+          handler: (data) => {
+            this.firestore.collection('treatment').doc(id).update({
+              Alarm: data.alert,
+            });
+            console.log('super');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { Observable, async } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface User {
+  birthdate: string;
+  bloodtype: string;
+  doctor: string;
+  height: number;
+  weight: number;
+}
 
 @Component({
   selector: 'app-profil',
   templateUrl: 'profil.page.html',
-  styleUrls: ['profil.page.scss']
+  styleUrls: ['profil.page.scss'],
 })
-export class ProfilPage {
+export class ProfilPage implements OnInit {
+  user$!: Observable<User>;
+  items: Observable<any[]>;
 
-  constructor() {}
 
+  constructor(public firestore: AngularFirestore) {
+    this.items = this.firestore.collection('treatment', ref => ref.where('Actual', '==', true)).valueChanges();
+  }
+  ngOnInit() {
+    this.user$ = this.firestore
+      .collection('users')
+      .doc('1')
+      .snapshotChanges()
+      .pipe(
+        map((doc) => {
+          const id = doc.payload.id;
+          const data = doc.payload.data() as User;
+          return { id, ...data };
+        })
+      );
+  }
 }
